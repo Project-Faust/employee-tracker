d0dc38a3ae82beb01ba8d220d122a1eb394b2c11
Project Faust <pentecost.cody.lane@gmail.com>
2023-04-17 18:59:30 -0400

    corrected syntax/clarity

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS 'role';
+DROP TABLE IF EXISTS employee_role;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
@@ -24,8 +24,7 @@ CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER,
-    FOREIGN KEY (role_id) REFERENCES role(id),
+    employee_role_id INTEGER NOT NULL,
+    FOREIGN KEY (employee_role_id) REFERENCES employee_role_id(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
