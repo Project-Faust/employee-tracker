dec7aa808e87792490eb2f443db145f36f2e0986
Project Faust <pentecost.cody.lane@gmail.com>
2023-04-17 18:21:09 -0400

    updated varchar value and arguments for columns

--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,28 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS 'role';
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER,
     fk_department FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
     manager_id INTEGER,
-    fk_role FOREIGN KEY (role_id) REFERENCES role(id),
-    fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
