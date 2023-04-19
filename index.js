74303971fe7efa0eb4e4a86af5dbc1ad5a1813a0
Project Faust <pentecost.cody.lane@gmail.com>
2023-04-18 21:49:19 -0400

    fixed error with prompt not waiting on user input

--- index.js
+++ index.js
@@ -1,58 +1,65 @@
 const db = require("./config/connection");
 const inquirer = require("inquirer");
-const consoleTable = require("console.table");
+// const consoleTable = require("console.table");
+const console = require("console");
+const promptFunctions = require("./utils/prompts")
+
 
 async function homePrompt() {
-    inquirer
-        .prompt(
-            {
-                type: "list",
-                message: "Please select an option from the list.",
-                choices: [
-                    "View All Departments",
-                    "View All Roles",
-                    "Add a Department",
-                    "Add a Role",
-                    "Add an Employee",
-                    "Update an Employee Role"
-                ]
-            }
-        )
-        .then(async (userResponse) => {
-            switch (userResponse.init) {
-                case "View All Departments":
-                    await getAllDepartments();
-                    homePrompt();
-                    break;
-                case "View All Roles":
-                    await getAllRoles();
-                    homePrompt();
-                    break;
-                case "View All Employees":
-                    await getAllEmployees();
-                    homePrompt();
-                    break;
-                case "Add a Department":
-                    await addDepartment();
-                    homePrompt();
-                    break;
-                case "Add a Role":
-                    await addRole();
-                    homePrompt();
-                    break;
-                case "Add an Employee":
-                    await addEmployee();
-                    homePrompt();
-                    break;
-                case "Update an Employee Role":
-                    await updateEmployeeRole();
-                    homePrompt();
-                    break;
-                default:
-                    console.log("Invalid selection. Please try again.");
-                    homePrompt();
-            };
+    let userResponse = null;
+    while (!userResponse) {
+        const answers = await inquirer.prompt({
+            name: "homePrompt",
+            type: "list",
+            message: "Please select an option from the list.",
+            choices: [
+                "View All Departments",
+                "View All Roles",
+                "Add a Department",
+                "Add a Role",
+                "Add an Employee",
+                "Update an Employee Role",
+                "Quit",
+            ],
         });
-};
+        userResponse = answers.homePrompt;
+    }
+
+    switch (userResponse) {
+        case "View All Departments":
+            promptFunctions.getAllDepartments();
+            homePrompt();
+            break;
+        case "View All Roles":
+            promptFunctions.getAllRoles();
+            homePrompt();
+            break;
+        case "View All Employees":
+            promptFunctions.getAllEmployees();
+            homePrompt();
+            break;
+        case "Add a Department":
+            promptFunctions.addDepartment();
+            homePrompt();
+            break;
+        case "Add a Role":
+            promptFunctions.addRole();
+            homePrompt();
+            break;
+        case "Add an Employee":
+            promptFunctions.addEmployee();
+            homePrompt();
+            break;
+        case "Update an Employee Role":
+            promptFunctions.updateEmployeeRole();
+            homePrompt();
+            break;
+        case "Quit":
+            process.exit();
+        default:
+            console.log("Invalid selection. Please try again.");
+            homePrompt();
+    }
+}
 
 homePrompt();
