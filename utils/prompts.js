1ff7d933bcb2a647be48970d8320534c7797bb91
Project Faust <pentecost.cody.lane@gmail.com>
2023-04-20 19:01:20 -0400

    created base functionality for adding department and employee/role

--- utils/prompts.js
+++ utils/prompts.js
@@ -1,20 +1,106 @@
+const { default: inquirer } = require("inquirer");
 const db = require("../config/connection");
 
-// correct structure to call inside inquirer prompt
+// shows all data in department table
 function getAllDepartments() {
     return db.promise().query(`SELECT * FROM department`);
 };
 
+// shows all data in employee_role table
 function getAllRoles() {
-    return db.query(`SELECT * FROM employee_role`)
+    return db.promise().query(`SELECT * FROM employee_role`)
 };
 
+// shows all data in the employee table
 function getAllEmployees() {
-    return db.query(`SELECT * FROM employee`)
+    return db.promise().query(`SELECT * FROM employee`)
+};
+
+// creates new row in department table
+async function addDepartment(newDepartment) {
+    await inquirer
+        .prompt(
+            {
+                name: "deptName",
+                type: "input",
+                message: "What is the name of the new department?"
+            }
+        )
+        .then(async (userInput) => {
+            db.query(`INSERT INTO department ${department_name} VALUES ${userInput.newDepartment}`);
+        })
+        .catch(console.log);
+};
+
+// finish multi-step thing for empRole
+
+// creates new row and fills columns in employee_role table
+async function addRole(newEmployeeRole) {
+    await inquirer
+        .prompt(
+            {
+                name: "empRoleName",
+                type: "input",
+                message: "What is the name of the new employee role?"
+            },
+            {
+                name: "empRoleSalary",
+                type: "number",
+                message: "What is the salary of the new role? (enter a number)"
+            },
+            {
+                name: "empRoleDept",
+                type: "list",
+                message: "To which department does this role belong?",
+                choices: 
+            }
+        )
+        .then(async (userInput) => {
+            db.query(`INSERT INTO employee_role ${title} VALUES ${userInput.newEmployeeRole}`);
+        })
+        .catch(console.log);
+};
+
+// finish multi-step thing for employee
+
+// creates new row and fills columns in employee table
+async function addEmployee(newEmployee) {
+    await inquirer
+        .prompt(
+            {
+                name: "newEmpFirstName",
+                type: "input",
+                message: "What is the first name of the new employee?"
+            },
+            {
+                name: "newEmpLastName",
+                type: "input",
+                message: "What is the last name of the new employee?"
+            },
+            {
+                name: "newEmpRole",
+                type: "list",
+                message: "What is the role of the new employee?"
+                choices: 
+            }
+
+        )
+        .then(async (userInput) => {
+            db.query(`INSERT INTO employee ${department_name} VALUES ${userInput.newEmployee}`);
+        })
+        .catch(console.log);
+};
+
+function updateEmployeeRole() {
+
 };
 
 module.exports = {
     getAllDepartments,
     getAllRoles,
-    getAllEmployees
+    getAllEmployees,
+    addDepartment,
+    addRole,
+    addEmployee,
+    updateEmployeeRole
 };
